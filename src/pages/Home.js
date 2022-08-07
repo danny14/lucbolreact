d06d582c413ccca57bbb30c476a7cdeab2f82c28
sebas991206 <sebas991206@hotmail.com>
2022-08-06 19:43:44 -0500

    NavBar and HeroSection

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,12 @@
-import NavScrollExample from './../components/NavBar';
+import NavScroll from './../components/NavBar';
+import HeroSection from './../components/HeroSection';
 
-export default function  Home(){
-    return <NavScrollExample/>;
+export default function Home(){
+    return (
+        <><div>
+            <NavScroll />
+        </div><div>
+                <HeroSection />
+            </div></>
+      );
 }
