8034514b4fb2f5b2f479cce6e29bf3536b609e6e
sebas991206 <sebas991206@hotmail.com>
2022-08-13 19:50:09 -0500

    New Page Footer

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import styles from './Form.module.css';
 import Row from 'react-bootstrap/Row';
@@ -7,39 +8,57 @@ import * as Icon from 'react-bootstrap-icons';
 import Form from 'react-bootstrap/Form';
 
 export default function Forms(){
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+
     return (
         <Container className={styles.FormContainer}>
-            <div className={styles.ReviewsBrand}>
-                <Row >
-                    <Col>
+            <div className={styles.FormTitle}>
+                <Row>
+                    <Col sm={6}>
                     <h3>CONTACT US</h3>
                     <h1>Send Us a Message</h1>
                     <p>Phone Email</p>
                     </Col>
-                    <Col>
-                        <Form>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Col sm={5}>
+                        <Form className={styles.FormStructure} noValidate validated={validated} onSubmit={handleSubmit}>
+                            <Form.Group className="mb-3" controlId="formName">
                                 <Form.Label>Full Name</Form.Label>
-                                <Form.Control type="email" placeholder="Full Name" />
-                                <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                                </Form.Text>
+                                <Form.Control required type="text" placeholder="Full Name" feedback="Full Name is a required field." feedbackType="invalid"/>
+                                <Form.Control.Feedback type="invalid">
+                                    Full Name is a required field.
+                                </Form.Control.Feedback>
                             </Form.Group>
-
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Group className="mb-3" controlId="formEmail">
                                 <Form.Label>Email</Form.Label>
-                                <Form.Control type="password" placeholder="Email" />
+                                <Form.Control required type="email" placeholder="name@example.com"/>
+                                <Form.Control.Feedback type="invalid">
+                                    Please choose a valid email.
+                                </Form.Control.Feedback>
                             </Form.Group>
-                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Group className="mb-3" controlId="formMessage">
                                 <Form.Label>Message</Form.Label>
-                                <Form.Control as="textarea" rows={3} />
+                                <Form.Control as="textarea" rows={2} placeholder="(Optional)"/>
                             </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                <Form.Check type="checkbox" label="Check me out" />
-                            </Form.Group>
-                            <Button variant="primary" type="submit">
-                                Submit
-                            </Button>
+                            <Form.Group className="mb-3" controlId="formTerms">
+                                <Row >
+                                    <Col>
+                                        <Form.Check type="checkbox" required label="Accept terms & conditions" feedback="You must agree before submitting." feedbackType="invalid"/>
+                                    </Col>
+                                    <Col>
+                                        <Button variant="primary" type="submit">Send <Icon.ArrowRight className={styles.IconForm}/></Button>
+                                    </Col>
+                                </Row>                                 
+                            </Form.Group>                            
                         </Form>
                     </Col>      
                 </Row>
