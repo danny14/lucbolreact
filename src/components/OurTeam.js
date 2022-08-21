8f33961d704c1b4d045ee7b89f0c249f304d6a3f
sebas991206 <sebas991206@hotmail.com>
2022-08-21 15:13:36 -0500

    Updates Responsive

--- src/components/OurTeam.js
+++ src/components/OurTeam.js
@@ -6,6 +6,7 @@ import LuisImage from './../assets/image/luccani-prueba-image.jpeg';
 import JaimeImage from './../assets/image/Jaime-Bolagay-image.png';
 import Card from 'react-bootstrap/Card';
 import {FormattedMessage} from 'react-intl';
+import LinkedinLogo from './../assets/image/linkedin-logo.png';
 
 export default function OurTeam(){
     return (
@@ -20,19 +21,19 @@ export default function OurTeam(){
             <Row className={styles.OurTeamCardsContainer + ' p-5 text-center'}>
                 <Col>
                     <Card className={styles.OurTeamCard + " mx-auto"}>
-                        <Card.Img variant="top" src={LuisImage} className='w-50 rounded-circle'/>
-                        <Card.Body className={styles.OurTeamCardBody +' w-50'}>
+                        <Card.Img variant="top" src={LuisImage} className={styles.OurTeamCardImg + ' rounded-circle'}/>
+                        <Card.Body className={styles.OurTeamCardBody}>
                             <Card.Title className={styles.OurTeamCardTitle}>Mr. Luis Luccani</Card.Title>
-                            <Card.Text className={styles.OurTeamCardText + ' text-center'}> <FormattedMessage id='ourTeam.luisTitle' defaultMessage="CEO of Vine" /> </Card.Text>
+                            <Card.Text className={styles.OurTeamCardText + ' text-center'}> <FormattedMessage id='ourTeam.luisTitle' defaultMessage="CEO of Vine" />  <a href="https://www.linkedin.com/in/luis-luccani-58b91b34" target="_blank" rel="noopener noreferrer"><img src={LinkedinLogo} alt='LinkedIn' className={styles.OurTeamCardLogo + ' d-inline-block align-top'} width='20' height='20'></img></a></Card.Text>
                         </Card.Body>
                     </Card>
                 </Col>
                 <Col>
                     <Card className={styles.OurTeamCard + " mx-auto"}>
-                        <Card.Img variant="top" src={JaimeImage} className='w-50 rounded-circle'/>
-                        <Card.Body className={styles.OurTeamCardBody +' w-50'}>
+                        <Card.Img variant="top" src={JaimeImage} className={styles.OurTeamCardImg + ' rounded-circle'}/>
+                        <Card.Body className={styles.OurTeamCardBody}>
                             <Card.Title className={styles.OurTeamCardTitle}>Mr. Jaime Bolagay</Card.Title>
-                            <Card.Text className={styles.OurTeamCardText + ' text-center'}> <FormattedMessage id='ourTeam.jaimeTitle' defaultMessage="Top Manager" /> </Card.Text>
+                            <Card.Text className={styles.OurTeamCardText + ' text-center'}> <FormattedMessage id='ourTeam.jaimeTitle' defaultMessage="Top Manager" />  <a href="https://www.linkedin.com/in/jaime-bolagay-26ba51190" target="_blank" rel="noopener noreferrer"><img src={LinkedinLogo} alt='LinkedIn' className={styles.OurTeamCardLogo + ' d-inline-block align-top'} width='20' height='20'></img></a></Card.Text>
                         </Card.Body>
                     </Card>
                 </Col>   
